9162886e80c4d956556db54cca653c5cb2734da6
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-07 14:31:23 -0400

    pycodestyle 2 blank lines after func def

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,6 +4,8 @@ def fizzbuzz():
 three, prints "Fizz" instead of the number. For multiples of five, prints
 "Buzz" instead of the number. For numbers which are multiples of both three
 and five, prints "FizzBuzz"."""
+
+
 for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz", end=" ")
