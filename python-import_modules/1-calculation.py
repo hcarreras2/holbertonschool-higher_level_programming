04e8d0c0e4a713a115d8389ecda97499e1f8e8e8
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-09 12:46:12 -0400

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__ ":
+if __name__ == "__main__":
     from calculator_1 import (add, sub, mul, div)
+
     a = 10
     b = 5
 
-
-def add(a+b):
     print("{} + {} = {}".format(a, b, add(a, b)))
-
-
-def sub(a-b):
     print("{} - {} = {}".format(a, b, sub(a, b)))
-
-
-def mul(a*b):
     print("{} * {} = {}".format(a, b, mul(a, b)))
-
-
-def div(a/b):
     print("{} / {} = {}".format(a, b, div(a, b)))
