9cb8b577c4552a67898de37aee04e787b1f531e8
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-09 23:01:53 -0400

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("Number of argument(s): 0.")
+    elif num_args == 1:
+        print(f"Number of argument(s): {num_args} argument:")
+    else:
+        print(f"Number of argument(s): {num_args} arguments:")
+
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print(f"{i}: {arg}")
