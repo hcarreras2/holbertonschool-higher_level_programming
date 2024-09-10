8bc1e2acf47e6513e232a70db9f566e3b8c3504c
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-09 23:10:49 -0400

    changed print stmnt

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,11 +4,11 @@ import sys
 if __name__ == "__main__":
     num_args = len(sys.argv) - 1
     if num_args == 0:
-        print("Number of argument(s): 0.")
+        print("0 arguments.")
     elif num_args == 1:
-        print(f"Number of argument(s): {num_args} argument:")
+        print(f"{num_args} argument:")
     else:
-        print(f"Number of argument(s): {num_args} arguments:")
+        print(f"{num_args} arguments:")
 
     for i, arg in enumerate(sys.argv[1:], start=1):
         print(f"{i}: {arg}")
