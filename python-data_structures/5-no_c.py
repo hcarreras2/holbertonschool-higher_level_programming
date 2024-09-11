0bdbf220bc04b0ba7fda4b1e585e3fa060dfb42a
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-11 00:30:09 -0400

    5-no_c.py

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    """Returns a string with all 'c' and 'C'
+    characters removed from the input string"""
+    my_string.remove('c', 'C')
+    return my_string
