18922dafdfb5925c99dcd83e057a6bbca18cc149
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-10 22:09:54 -0400

    0-print_list_integer.py

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """Prints all integers in a list."""
+numbers = 0
+    for line in my_list[0]:
+        numbers = numbers + 1
+        print(line)
