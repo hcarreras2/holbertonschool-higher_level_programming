ecc9069b433277a2b0fd4f98dd16f47e4f8a9681
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-10 23:01:50 -0400

    pycodstyle

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,8 +2,6 @@
 
 def replace_in_list(my_list, idx, element):
     """Replace an element in a list at a specific index."""
-    
-    
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
