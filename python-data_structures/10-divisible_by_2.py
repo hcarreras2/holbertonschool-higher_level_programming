39a01b169fd3c2fcc304dd4d14270837c1358fb0
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-11 02:03:37 -0400

    10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,4 +3,9 @@
 def divisible_by_2(my_list=[]):
     """Returns a list of all integers in the input list
     that are divisible by 2."""
-    return [i for i in my_list if i % 2 == 0]
+    i = 0
+    if i in my_list:
+        i % 2 == 0
+        return True
+    else:
+        return False
