486c553de5b8808a269ff8eba25cc0a1e2e18dba
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-11 01:02:55 -0400

    another space?

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
     """Prints a matrix of integers."""
     for row in matrix:
         for num in row:
-            print("{:d} ".format(num), end="")
+            print("{:d}".format(num), end=" ")
         print()
