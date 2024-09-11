1408d8b61a44b06ca6c1e8a4f5e55be9697171e4
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-10 23:40:33 -0400

    new var for new list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,5 +4,9 @@ def new_in_list(my_list, idx, element):
     """Replaces an element in a list at a specific index."""
     if idx < 0 or idx >= len(my_list):
         return my_list
-    my_list[idx] = element
+
+    new_list = my_list
+
+    else new_list[idx] = element
+    return new_list
     return my_list
