27666ebfa9557baee8db233e35f3e75de8d9a5f6
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-11 00:05:05 -0400

    4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,11 +3,11 @@
 def new_in_list(my_list, idx, element):
     """Replaces an element in a list at a specific
     index without modifying the original."""
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
 
     new_list = my_list.copy()
 
     new_list[idx] = element
 
-    return my_list, new_list
+    return new_list
