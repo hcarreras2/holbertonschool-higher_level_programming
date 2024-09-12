80f08bad918060827d0d5735bc53d0eac9e3b5b3
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-11 23:01:40 -0400

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    """Returns a key with the biggest integer if it exists in the 
+    """Returns a key with the biggest integer if it exists in the
     dictionary,otherwise None"""
     if a_dictionary and a_dictionary.values():
         return max(a_dictionary, key=a_dictionary.get)
