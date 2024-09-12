cdc6b37f2f4fa67fe406661abaaa07a182191206
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-12 19:11:50 -0400

    2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -10,9 +10,12 @@ def safe_print_list_integers(my_list=[], x=0):
     Returns:
     None
     """
+    numbers = 0
     for i in range(x):
         try:
-            print(int(my_list[i]))
+            print("{:d}".format(my_list[i]), end="")
+            numbers += 1
             except (ValueError, IndexError):
-pass
-return None
+                pass
+    print()
+    return numbers
