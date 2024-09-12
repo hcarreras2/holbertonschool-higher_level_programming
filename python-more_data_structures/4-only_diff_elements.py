d213e7f7c6fcf08f4019dd896fc47c46a825b0f5
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-11 22:33:20 -0400

    4-only_diff_elements.py

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    """Return a set of elements that are in set_1 but not in set_2."""
-    return set_1 - set_2, set_2 - set_1
+    """Return a set of elements that are in set_1 but not in set_2
+    and viceversa"""
+    return set_1.symmetric_difference(set_2)
