9d159aef659d9eebf356ab915c0105ccdfe87752
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-19 02:04:43 -0400

    pycodestyle

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,6 +2,7 @@
 
 """ This class defines a square"""
 
+
 class Square:
     """This is the class named square"""
     def __init__(self, size=0):
@@ -15,7 +16,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     def area(self):
         """Returns the current square area"""
         return self.__size ** 2
