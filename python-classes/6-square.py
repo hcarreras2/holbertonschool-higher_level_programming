7043283850a4da6987df498077db5bb5de583c6a
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-19 15:36:05 -0400

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -21,15 +21,6 @@ class Square:
         """
         return self.__size
 
-    @position.setter
-    def position(self, value):
-        """This is a setter method for position
-        of the square with validation."""
-        if not isinstance(value, tuple) or len(value) != 2 \
-            or not all(isinstance(num, int) and num >= 0 for num in value):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
-
     @size.setter
     def size(self, value):
         """This is a method to set the size of the square
@@ -46,6 +37,20 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    @property
+    def position(self):
+        """This retreives the position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """This is a setter method for position
+        of the square with validation."""
+        if not isinstance(value, tuple) or len(value) != 2 \
+                or not all(isinstance(num, int) and num >= 0 for num in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """This is a method to calculate the area of the square
@@ -66,9 +71,6 @@ class Square:
         if self.size == 0:
             print("")
             return
-
-         [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+        print("\n" * self.position[1], end="")
+        for s in range(self.size):
+            print(" " * self.position[0] + "#" * self.size)
