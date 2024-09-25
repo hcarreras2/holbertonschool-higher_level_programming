08229b298251336e5f350d14b345d54118eaba1a
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-25 14:08:00 -0400

    some changes for pep8

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -2,11 +2,12 @@
 
 """A function to know if isinstance"""
 
+
 def is_same_class(obj, a_class):
     """Returns True if an object is exactly an instance of
     the specified class; otherwise false"""
 
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     else:
         return False
