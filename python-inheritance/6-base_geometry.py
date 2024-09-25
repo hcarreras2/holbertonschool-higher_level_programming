b1f2a928897ce97aac9daee24981ea10778d817b
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-25 14:40:12 -0400

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """raises Exception with the message:
+    area() is not implemented"""
+
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
