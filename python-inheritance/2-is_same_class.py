7fcc1a0823e6295f5f7b5e19173960dcee4ead5a
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-25 14:05:57 -0400

    used type instead of isinstance

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,8 +5,8 @@
 def is_same_class(obj, a_class):
     """Returns True if an object is exactly an instance of
     the specified class; otherwise false"""
-    
-    if isinstance(obj, a_class):
+
+    if type(obj) == a_class:
         return True
     else:
         return False
