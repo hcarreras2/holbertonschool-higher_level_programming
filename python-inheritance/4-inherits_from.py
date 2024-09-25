06eaae6115e509bf62c64a7f5bf78afaff5191be
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-25 14:22:44 -0400

    added type to make sure its an instance of a subclass

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -13,7 +13,7 @@ def inherits_from(obj, a_class):
     directly or indirectly from the class, False otherwise
     """
 
-    if issubclass(obj, a_class):
+    if issubclass(obj, a_class) and type(obj) is not a_class:
         return True
     else:
         return False
