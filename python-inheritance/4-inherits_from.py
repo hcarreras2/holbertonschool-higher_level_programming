c499135842895fbf0fc3f11cd82553143ae3c032
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-25 14:26:18 -0400

    4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -13,4 +13,7 @@ def inherits_from(obj, a_class):
     directly or indirectly from the class, False otherwise
     """
 
-    return issubclass(obj, a_class) and type(obj) is not a_class
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
