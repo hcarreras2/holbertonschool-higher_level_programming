aa556fa3f618d29515e87f2d9db35d8d03d998a9
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-09-30 10:31:11 -0400

    000

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,20 @@
+#!/bin/usr/python3
+"""Abstract class named Animal"""
+
+from abc import ABC
+class Animal(ABC):
+    """Abstract class Animal"""
+
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Dog(Animal):
+    """Subclass Dog"""
+    def sound(self):
+        return "Bark"
+    
+class Cat(Animal):
+    """Subclass Cat"""
+    def sound(self):
+        return "Meow"
