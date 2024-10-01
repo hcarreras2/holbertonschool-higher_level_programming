cd26de991e53bbb85cc306e0a12c170076db19a7
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-10-01 00:12:16 -0400

    task_02_verboselist.py

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""Class named VerboseList that extend list class"""
+
+
+class VerboseList(list):
+    """Class that extend list class
+    prints a notification message every time an item is added
+    or removed"""
+
+    def append(self, item):
+        """append method that print a notification message"""
+        super().append(item)
+        print("Added [{}] to the list".format(item))
+
+    def extetend(self, item):
+        """extend method that print a notification message with
+        numbers of items added"""
+        super().extend(item)
+        print("Extended the list with [{}] items".format(len(item)))
+
+    def remove(self, item):
+        """
+        Before removing the item from the list,
+        print a message like “Removed [item] from the list.”
+        """
+        super().remove(item)
+        print("Removed [{}] from the list".format(item))
+
+    def pop(self, item):
+        """
+        Before popping the item from the list,
+        print a message like “Popped [item] from the list.”
+        If the index is not specified, default behavior
+        is to pop the last item.
+        """
+        super().pop(item)
+        print("Popped [{}] from the list".format(item))
