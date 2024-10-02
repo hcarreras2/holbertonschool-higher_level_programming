1a23956ecf8bc42433e57fd6d3d6c0cef0945132
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-10-02 16:07:29 -0400

    2-append_write.py

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,22 @@
+#!usr/bin/python3
+
+"""Append a string to the end of a text file and
+return the number of characters added"""
+
+
+def append_write(filename="", text=""):
+    """Append a string to the end of a text file and
+        return the number of characters added.
+
+    If the file doesn't exist, it is created.
+
+    Args:
+        filename (str): The name of the file to write to.
+        text (str): The text to write to the file.
+
+    Returns:
+        int: The number of characters added to the file.
+    """
+    with open(filename, "a") as file:
+        added_chars = file.write(text)
+        return added_chars
