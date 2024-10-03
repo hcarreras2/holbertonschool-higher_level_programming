afe000c0b48733a36c1d328bef30eb34943799bf
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-10-03 14:16:30 -0400

    task_03_xml.py

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,26 @@
+#!usr/bin/python3
+
+import xml.etree.ElementTree as ET
+
+"""Serialization and deserialization using XML as an alternative format to JSON."""
+
+
+def serialize_to_xml(dictionary, filename):
+    """Serialize a dictionary to an XML file."""
+    root = ET.Element("root")
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, key)
+        child.text = str(value)
+        tree = ET.ElementTree(root)
+        tree.write(filename)
+        return root
+
+def desirialize_from_xml(filename):
+    """Deserialize an XML file to a dictionary."""
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    dictionary = {}
+    for child in root:
+        dictionary[child.tag] = child.text
+        return dictionary
+
