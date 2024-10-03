8851c3e375042e1899d5c71e33f13515638f8e8b
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-10-02 22:42:01 -0400

    11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""Class named Student"""
+
+
+class Student:
+    """Defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a dictionary representation of the student"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the student"""
+        for key, value in json.items():
+            setattr(self, key, value)
+            return self
+        return self
