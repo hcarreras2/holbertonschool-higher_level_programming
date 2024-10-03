136d1b8813093aea54fb1be69bdb2b29c0d70025
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-10-03 14:23:17 -0400

    task_03_xml.py

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -2,7 +2,8 @@
 
 import xml.etree.ElementTree as ET
 
-"""Serialization and deserialization using XML as an alternative format to JSON."""
+"""Serialization and deserialization using XML as an alternative
+format to JSON."""
 
 
 def serialize_to_xml(dictionary, filename):
@@ -12,8 +13,8 @@ def serialize_to_xml(dictionary, filename):
         child = ET.SubElement(root, key)
         child.text = str(value)
     tree = ET.ElementTree(root)
-    with open(filename, 'wb') as xml_file:
-        tree.write(xml_file)
+    tree.write(xml_file)
+
 
 def desirialize_from_xml(filename):
     """Deserialize an XML file to a dictionary."""
@@ -22,5 +23,4 @@ def desirialize_from_xml(filename):
     dictionary = {}
     for child in root:
         dictionary[child.tag] = child.text
-        return dictionary
-
+    return dictionary
