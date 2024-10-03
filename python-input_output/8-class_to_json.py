8c517de7843374790937844de66337e2a3a7f688
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-10-02 21:41:31 -0400

    8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -9,4 +9,4 @@ def class_to_json(obj):
     """Returns a dictionary description with simple data
     structure for JSON serialization of an object"""
 
-    return obj.to_json_string()
+    return obj.__dict__
