a849a83d2b8dff2e118492f7756c89d5ceee49ef
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-10-03 14:26:47 -0400

    changed root to data

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -8,18 +8,20 @@ format to JSON."""
 
 def serialize_to_xml(dictionary, filename):
     """Serialize a dictionary to an XML file."""
-    root = ET.Element("root")
+    root = ET.Element("data")
     for key, value in dictionary.items():
         child = ET.SubElement(root, key)
         child.text = str(value)
     tree = ET.ElementTree(root)
-    tree.write(xml_file)
+    with open(filename, 'wb') as xml_file:
+        tree.write(xml_file, encoding='utf-8', xml_declaration=True)
 
 
 def desirialize_from_xml(filename):
     """Deserialize an XML file to a dictionary."""
     tree = ET.parse(filename)
     root = tree.getroot()
+
     dictionary = {}
     for child in root:
         dictionary[child.tag] = child.text
