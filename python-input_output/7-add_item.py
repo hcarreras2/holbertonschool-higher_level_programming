8dfc5e3fa19b1f0400cf23f6412748e1e00335e7
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-10-02 21:13:20 -0400

    indent

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,8 +9,9 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if os.path.exists("add_item.json"):
-        items = load_from_json_file("add_item.json")
+    items = load_from_json_file("add_item.json")
 else:
     items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")  # save to file
+
+items.extend(sys.argv[1:])
+save_to_json_file(items, "add_item.json")  # save to file
