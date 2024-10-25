c1cfbd67dc27987230d9142ea053f687bd31bc7b
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-10-25 17:13:19 -0400

    5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
--- Display the column information for the table first_table in the specified database
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM information_schema.columns
-WHERE table_name = 'first_table' AND table_schema = DATABASE();
+-- Script to display the creation details of 'first_table'
+SHOW CREATE TABLE first_table;
