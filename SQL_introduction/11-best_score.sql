ff61318ea86712b754fc4c1b5910c772d361e2a2
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-10-25 23:54:10 -0400

    11-best_score.sql

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Select the score and name from second_table where score is greater than or equal to 10
+-- Order the results by score in descending order (highest score first)
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
