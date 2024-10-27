28e217fa66818f563ee7651d94971b92233664ed
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-10-27 02:43:38 -0400

    1-create_user.sql

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Create user_0d_1 with all privileges if it does not already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Flush privileges to ensure that the changes take effect
+FLUSH PRIVILEGES;
