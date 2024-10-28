0e509dddb86ae4ef674a82af24d74d58c35f0b2d
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-10-27 23:03:18 -0400

    6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,8 @@
 -- A script that creates the database hbtn_0d_usa and the table cities.
 CREATE DATABASE IF NOT EXIST hbtn_0d_usa;
-CREATE TABLE IF NOT EXIST cities(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXIST states(
+    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id)
