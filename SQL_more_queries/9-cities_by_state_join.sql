cdd8c30a8b1e2527471c67ec4332bb7632393a64
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-10-27 23:24:07 -0400

    9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,5 @@
--- Script that lists all cities with their states in the database "hbtn_0d_usa"
-SELECT cities.id, cities.name, (
-    SELECT name
-    FROM states
-    WHERE states.id = cities.state_id
-) AS state_name
+-- Script to list all cities with their states in "hbtn_0d_usa" database
+SELECT cities.id, cities.name, states.name
 FROM cities
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
